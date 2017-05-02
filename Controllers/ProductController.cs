3f8af88058138e7227f12ac4a0e006a12e3f0d4c
Reed <Reeddenny@gmail.com>
2017-05-02 09:54:49 -0700

    NewNew

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -39,62 +39,6 @@ namespace eCommerceReloaded.Controllers
         public IActionResult addproduct(ProductValidation aProduct)
          {
              //aProduct is the validation instantiated object, if valid add to Product Model 
-           if(ModelState.IsValid)
-           {
-               //categoryID starts at 0 which means that if it stays at 0, 
-               //there is no category with that name after the check(see check below)
-               int categoryId = 0;
-               
-               foreach(Category aCategory in _context.categories.ToList())
-               {
-                   //this is the check
-                   if(aCategory.name == aProduct.name)
-                   {
-                       categoryId = aCategory.categoryId;
-                   }
-               }
-               //if nothing is found, add new category
-               if(categoryId == 0)
-               {
-                   Category newCategory = new Category
-                   {
-                       name = aProduct.name,
-                       created_At = DateTime.Now,
-
-                   };
-                   _context.categories.Add(newCategory);
-                   //updated parent variable categoryId with new categoryID
-                   categoryId = newCategory.categoryId;
-                   
-               }
-               else 
-               {
-                   //if the category name already exists pull up the category when adding inventory
-                   Category existingCategory = _context.categories 
-                   .SingleOrDefault(category => category.categoryId == categoryId);
-                   categoryId = existingCategory.categoryId;
-               }
- 
-               Product newProduct = new Product 
-               {
-                   //created new product 
-                name = aProduct.name,
-                image = aProduct.image,
-                inventory = aProduct.quantity,
-                cost = aProduct.cost,
-                price = aProduct.price,
-                created_At = DateTime.Now,
-                categoryId = categoryId,
-               };
-               _context.Add(newProduct);
-           }
-           //finally save changes to Database
-           _context.SaveChanges();
-             return View("productadmin");
-         }
-     }
-=======
-        {
             //aProduct is the validation instantiated object, if valid add to Product Model 
             if(ModelState.IsValid)
             {
@@ -136,7 +80,7 @@ namespace eCommerceReloaded.Controllers
                 {
                     //created new product 
                  name = aProduct.name,
-                 imageUrl = aProduct.imageUrl,
+                 image = aProduct.image,
                  inventory = aProduct.quantity,
                  cost = aProduct.cost,
                  price = aProduct.price,
@@ -156,6 +100,5 @@ namespace eCommerceReloaded.Controllers
             return View("productadmin");
             // return View("productadmin");
         }
->>>>>>> Reed
     }
 }
